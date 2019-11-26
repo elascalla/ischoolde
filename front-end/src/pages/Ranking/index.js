acc923b2140537f5dbe3ff844656c9b3af15efcc
Jardel Kuhn <jardelkuhn@gmail.com>
2019-11-26 16:42:48 -0300

    chore: ranking list

--- front-end/src/pages/Ranking/index.js
+++ front-end/src/pages/Ranking/index.js
@@ -1,7 +1,40 @@
 import React from "react";
 
-// import { Container } from './styles';
+import { StudentWrapper, ViewStudentButton, Container } from "./styles";
 
 export default function Ranking() {
-  return <div />;
+  const data = [
+    {
+      name: "João das Neves",
+      email: "joao@gmail.com",
+      exp: 40,
+      challengesCompleted: 121
+    },
+    {
+      name: "João das Neves",
+      email: "joao@gmail.com",
+      exp: 40,
+      challengesCompleted: 121
+    }
+  ];
+
+  return (
+    <Container>
+      <ul>
+        {data.map(student => (
+          <StudentWrapper>
+            <div>
+              <p>{student.name}</p>
+              <p>{student.email}</p>
+            </div>
+            <div>
+              <p>{student.exp}</p>
+              <p>{student.challengesCompleted}</p>
+            </div>
+            <ViewStudentButton />
+          </StudentWrapper>
+        ))}
+      </ul>
+    </Container>
+  );
 }
