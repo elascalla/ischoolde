1ae051159523185baaa3c6d25914a4a1467f4291
Gustavo <gsinacio94@gmail.com>
2019-11-26 14:18:52 -0300

    Master fix

--- front-end/src/pages/Dashboard/index.js
+++ front-end/src/pages/Dashboard/index.js
@@ -1,76 +1,75 @@
-import React, { Component } from 'react';
-import { MdAddShoppingCart } from 'react-icons/md';
+import React from "react";
+import { MdAddShoppingCart } from "react-icons/md";
 
-import { ProductList } from './styles';
+import { ProductList } from "./styles";
 
-export default function Dashboard(){
+export default function Dashboard() {
+  return (
+    <ProductList>
+      <li key="1">
+        <img
+          src="https://i0.wp.com/blog.grandchef.com.br/wp-content/uploads/2018/09/Logo-iFood.jpg?ssl=1"
+          alt="Desafio iFood"
+        />
+        <strong>Desafio iFood</strong>
+        <span>NIVEL 2</span>
 
-    return (
-      <ProductList>
-        <li key="1">
-          <img src="https://i0.wp.com/blog.grandchef.com.br/wp-content/uploads/2018/09/Logo-iFood.jpg?ssl=1" alt="Desafio iFood" />
-          <strong>Desafio iFood</strong>
-          <span>NIVEL 2</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 2
+          </div>
 
-          <button
-            type="button"
-          >
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" />{' '}
-              2
-            </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li key="1">
+        <img
+          src="https://i0.wp.com/blog.grandchef.com.br/wp-content/uploads/2018/09/Logo-iFood.jpg?ssl=1"
+          alt="Desafio iFood"
+        />
+        <strong>Desafio iFood</strong>
+        <span>NIVEL 2</span>
 
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-        <li key="1">
-          <img src="https://i0.wp.com/blog.grandchef.com.br/wp-content/uploads/2018/09/Logo-iFood.jpg?ssl=1" alt="Desafio iFood" />
-          <strong>Desafio iFood</strong>
-          <span>NIVEL 2</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 2
+          </div>
 
-          <button
-            type="button"
-          >
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" />{' '}
-              2
-            </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li key="1">
+        <img
+          src="https://i0.wp.com/blog.grandchef.com.br/wp-content/uploads/2018/09/Logo-iFood.jpg?ssl=1"
+          alt="Desafio iFood"
+        />
+        <strong>Desafio iFood</strong>
+        <span>NIVEL 2</span>
 
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-        <li key="1">
-          <img src="https://i0.wp.com/blog.grandchef.com.br/wp-content/uploads/2018/09/Logo-iFood.jpg?ssl=1" alt="Desafio iFood" />
-          <strong>Desafio iFood</strong>
-          <span>NIVEL 2</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 2
+          </div>
 
-          <button
-            type="button"
-          >
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" />{' '}
-              2
-            </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li key="1">
+        <img
+          src="https://i0.wp.com/blog.grandchef.com.br/wp-content/uploads/2018/09/Logo-iFood.jpg?ssl=1"
+          alt="Desafio iFood"
+        />
+        <strong>Desafio iFood</strong>
+        <span>NIVEL 2</span>
 
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-        <li key="1">
-          <img src="https://i0.wp.com/blog.grandchef.com.br/wp-content/uploads/2018/09/Logo-iFood.jpg?ssl=1" alt="Desafio iFood" />
-          <strong>Desafio iFood</strong>
-          <span>NIVEL 2</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 2
+          </div>
 
-          <button
-            type="button"
-          >
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" />{' '}
-              2
-            </div>
-
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-      </ProductList>
-    );
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
--- front-end/src/routes/Route.js
+++ front-end/src/routes/Route.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Route, Redirect } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { Route, Redirect } from "react-router-dom";
 
-import AuthLayout from '../pages/_layouts/auth';
-import DefaultLayout from '../pages/_layouts/default';
-
-import { store } from '../store';
+import DefaultLayout from "../pages/_layouts/default";
 
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const { signed } = store.getState().auth;
+  const { signed } = true;
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
@@ -38,10 +35,9 @@ export default function RouteWrapper({
 
 RouteWrapper.propTypes = {
   isPrivate: PropTypes.bool,
-  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
-    .isRequired,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func]).isRequired
 };
 
 RouteWrapper.defaultProps = {
-  isPrivate: false,
+  isPrivate: false
 };
