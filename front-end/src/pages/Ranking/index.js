d8d3ddc2cb1b4b9c040306199035e2e14c80206a
Jardel Kuhn <jardelkuhn@gmail.com>
2019-11-26 17:22:37 -0300

    chore: finished student ranking

--- front-end/src/pages/Ranking/index.js
+++ front-end/src/pages/Ranking/index.js
@@ -5,16 +5,54 @@ import { StudentWrapper, ViewStudentButton, Container } from "./styles";
 export default function Ranking() {
   const data = [
     {
-      name: "João das Neves",
+      name: "João",
       email: "joao@gmail.com",
-      exp: 40,
-      challengesCompleted: 121
+      exp: 40
     },
     {
-      name: "João das Neves",
-      email: "joao@gmail.com",
-      exp: 40,
-      challengesCompleted: 121
+      name: "Estanis",
+      email: "estanis@gmail.com",
+      exp: 12
+    },
+    {
+      name: "Fernando",
+      email: "fernando@gmail.com",
+      exp: 12
+    },
+    {
+      name: "Daniel",
+      email: "daniel@gmail.com",
+      exp: 12
+    },
+    {
+      name: "Jonas",
+      email: "jonas@gmail.com",
+      exp: 12
+    },
+    {
+      name: "Daniela",
+      email: "daniela@gmail.com",
+      exp: 12
+    },
+    {
+      name: "Maria",
+      email: "Maria@gmail.com",
+      exp: 12
+    },
+    {
+      name: "Estanis Barão",
+      email: "estanis@gmail.com",
+      exp: 12
+    },
+    {
+      name: "Estanis Barão",
+      email: "estanis@gmail.com",
+      exp: 12
+    },
+    {
+      name: "Estanis Barão",
+      email: "estanis@gmail.com",
+      exp: 12
     }
   ];
 
@@ -23,14 +61,18 @@ export default function Ranking() {
       <ul>
         {data.map(student => (
           <StudentWrapper>
+            <img
+              src={"https://api.adorable.io/avatars/50/abott@adorable.png"}
+            />
+
             <div>
               <p>{student.name}</p>
               <p>{student.email}</p>
             </div>
             <div>
-              <p>{student.exp}</p>
-              <p>{student.challengesCompleted}</p>
+              <p>Nível: {student.exp}</p>
             </div>
+            <button type="button">VER PERFIL</button>
             <ViewStudentButton />
           </StudentWrapper>
         ))}
--- front-end/src/pages/Ranking/styles.js
+++ front-end/src/pages/Ranking/styles.js
@@ -14,21 +14,40 @@ export const Container = styled.div`
 `;
 
 export const StudentWrapper = styled.li`
-  background-color: #ccc;
-
+  background: linear-gradient(-120deg, #0f0f17, #1f1f2b);
+  color: #fff;
   margin: 0 0 8px 0;
-  width: 900px;
+  width: 600px;
   padding: 20px;
   border-radius: 6px;
   display: flex;
   flex-direction: row;
-  align-items: center;
   justify-content: space-around;
 
   div {
     display: flex;
+    padding: 5px;
+
+    p:first-child {
+      font-size: 17px;
+      font-weight: bold;
+    }
+
     flex-direction: column;
-    align-items: center;
+    justify-content: center;
+  }
+
+  button {
+    background-color: #fff;
+    color: #0f0f17;
+    border-radius: 4px;
+    border: 0;
+    padding: 10px;
+    display: flex;
+    justify-content: space-around;
+    width: 140px;
+    text-justify: auto;
+    font-weight: bold;
   }
 `;
 
